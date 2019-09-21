b46c9c55d4ca42aed6e020d27dc269c1a1b8351b
Student <student@itpark.ru>
2019-09-21 10:13:43 +0300

    Doubles removed fixes #1

--- app.js
+++ app.js
@@ -11,21 +11,18 @@ if (firstVote >= 9) {
 if (firstVote <= 6) {
     detractorsCount = detractorsCount + 1;
 }
-
 if (secondVote >= 9) {
     promoutersCount = promoutersCount + 1;
 }
 if (secondVote <= 6) {
     detractorsCount = detractorsCount + 1;
 }
-
 if (thirdVote >= 9) {
     promoutersCount = promoutersCount + 1;
 }
 if (thirdVote <= 6) {
     detractorsCount = detractorsCount + 1;
 }
-
 const nps = (promoutersCount / 3 - detractorsCount / 3) * 100;
 
 console.log(nps);
